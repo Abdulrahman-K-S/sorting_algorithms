b04e9be3199fbe069fd3137b176772f90d357f2d
Abdulrahman Khaled <AK-Salah@outlook.com>
2023-11-18 03:32:58 -0800

    Added '0-bubble_sort.c' & '0-O'
    0-bubble_sort.c:
    - Contains the bubble sorting algorithm
    
    0-O:
    - Contains the
     - Best Case
     - Average Case
     - Worst Case

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - A function that sorts an array of integers in
+ *               ascending order using the Bubble sort algorithm.
+ *
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ *
+ * Return: Nothing.
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+				print_array(array, size);
+			}
+		}
+}
