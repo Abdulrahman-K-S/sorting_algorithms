70ffb5fbcd9a6317857ee9e049323afe13374ed7
Abdulrahman Khaled <AK-Salah@outlook.com>
2023-11-25 08:40:10 -0800

    Added advanced function prototypes

--- sort.h
+++ sort.h
@@ -28,5 +28,10 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+/* Advanced Algorithms */
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
 
 #endif
